c58702b24fb48c2f7691e031a72dd2d16b232c15
Jorge Salas Leybon <146293962+JorSL1120@users.noreply.github.com>
2025-09-26 01:42:20 -0600

    ya cree todos los scripts

---
+++ KeeperKingProject/Assets/JorgeSalas/Scripts/SettingsUI.cs
@@ -0,0 +1,46 @@
+using System;
+using Dino.UtilityTools.Extensions;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class SettingsUI : UIWindow
+{
+    #region Popup implementation
+    
+    #region serialized Fields
+    [Header("Popup Settings")]
+    [SerializeField] private Button _buttonBack;
+    [SerializeField] private Button _buttonCredits;
+    [SerializeField] private Button _buttonMenu;
+    [SerializeField] private Button _buttonQuit;
+    #endregion
+    
+    public override void Initialize()
+    {
+        base.Initialize();
+        _buttonBack.onClick.AddListener(BackClick);
+        _buttonCredits.onClick.AddListener(CreditsClick);
+        _buttonMenu.onClick.AddListener(MenuClick);
+        _buttonQuit.onClick.AddListener(QuitClick);
+    }
+    private void BackClick()
+    {
+        Hide();
+        Debug.Log("Back clicked");
+    }
+    private void CreditsClick()
+    {
+        Debug.Log("Credits Clicked");
+    }
+    private void MenuClick()
+    {
+        Debug.Log("Menu Clicked");
+    }
+    private void QuitClick()
+    {
+        Debug.Log("Quit clicked");
+    }
+    #endregion
+}
