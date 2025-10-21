696fa050a1194805306ad93f3345551aaad16beb
Jorge Salas Leybon <146293962+JorSL1120@users.noreply.github.com>
2025-10-21 01:23:40 -0600

    arreglando cosas

--- KeeperKingProject/Assets/JorgeSalas/Scripts/UI/Responsive/ResponsiveElement.cs
+++ KeeperKingProject/Assets/JorgeSalas/Scripts/UI/Responsive/ResponsiveElement.cs
@@ -1,11 +1,9 @@
 using NaughtyAttributes;
 using UnityEngine;
-using NaughtyAttributes;
 
 
 public class ResponsiveElement : MonoBehaviour
 {
-<<<<<<< HEAD
     [Header("Settings")]
     [SerializeField] private RectTransform rectTransform;
  
@@ -19,27 +17,10 @@ public class ResponsiveElement : MonoBehaviour
     
     ResponsiveManager _responsiveManager;
     
-=======
-
-    [Header("Settings")]
-    [SerializeField] private RectTransform rectTransform;
-
-    [Header("Mobile Anchors")]
-    [SerializeField] private Vector2 mobileAnchorMin = new Vector2(0, 0);
-    [SerializeField] private Vector2 mobileAnchorMax = new Vector2(0, 0);
-
-    [Header("Tablet Anchors")]
-    [SerializeField] private Vector2 tabletAnchorMin = new Vector2(0, 0);
-    [SerializeField] private Vector2 tabletAnchorMax = new Vector2(0, 0);
-
-    ResponsiveManager _responsiveManager;
-
->>>>>>> cd08fa84f4428953db06be15fc192eb905ccd509
     void Start()
     {
         _responsiveManager = ResponsiveManager.Instance;
         _responsiveManager.OnScreenSizeChanged.AddListener(UpdateAnchors);
-<<<<<<< HEAD
         UpdateAnchors();    
     }
     
@@ -61,38 +42,11 @@ public class ResponsiveElement : MonoBehaviour
     
     private void SetTabletAnchors()
     {
-=======
-        UpdateAnchors();
-    }
-
-
-    public void UpdateAnchors()
-    {
-        if (_responsiveManager == null) return;
-
-        if (_responsiveManager.CurrentDeviceType == DeviceType.Mobile)
-        {
-            SetMobileAnchors();
-        }
-        else if (_responsiveManager.CurrentDeviceType == DeviceType.Tablet)
-        {
-            SetTabletAnchors();
-        }
-    }
-
-
-    private void SetTabletAnchors()
-    {
->>>>>>> cd08fa84f4428953db06be15fc192eb905ccd509
         rectTransform.anchorMin = tabletAnchorMin;
         rectTransform.anchorMax = tabletAnchorMax;
     }
 
-<<<<<<< HEAD
    
-=======
-
->>>>>>> cd08fa84f4428953db06be15fc192eb905ccd509
     private void SetMobileAnchors()
     {
         rectTransform.anchorMin = mobileAnchorMin;
@@ -104,29 +58,17 @@ public class ResponsiveElement : MonoBehaviour
     {
         Vector2 maxAnchors = rectTransform.anchorMax;
         Vector2 minAnchors = rectTransform.anchorMin;
-<<<<<<< HEAD
         
         mobileAnchorMax = maxAnchors;
         mobileAnchorMin = minAnchors;
     }
     
-=======
-
-        mobileAnchorMax = maxAnchors;
-        mobileAnchorMin = minAnchors;
-    }
-
->>>>>>> cd08fa84f4428953db06be15fc192eb905ccd509
     [Button]
     private void SaveTabletAnchors()
     {
         Vector2 maxAnchors = rectTransform.anchorMax;
         Vector2 minAnchors = rectTransform.anchorMin;
-<<<<<<< HEAD
         
-=======
-
->>>>>>> cd08fa84f4428953db06be15fc192eb905ccd509
         tabletAnchorMax = maxAnchors;
         tabletAnchorMin = minAnchors;
     }
